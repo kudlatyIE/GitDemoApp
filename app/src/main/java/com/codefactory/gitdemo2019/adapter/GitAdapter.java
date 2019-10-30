f2a6e1aef328324402e2f1d66c2f1418c463863e
Nikodem Walicki <afsnicodemwalicki@gmail.com>
2019-10-30 17:50:21 +0100

    implementation view in progres

---
+++ app/src/main/java/com/codefactory/gitdemo2019/adapter/GitAdapter.java
@@ -0,0 +1,24 @@
+package com.codefactory.gitdemo2019.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GitAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/codefactory/gitdemo2019/adapter/GitViewHolder.java
@@ -0,0 +1,13 @@
+package com.codefactory.gitdemo2019.adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GitViewHolder extends RecyclerView.ViewHolder {
+
+    public GitViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
